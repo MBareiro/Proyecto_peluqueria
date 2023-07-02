4f9c1d5aff1ccdd042832c785cc6412b3d468e91
MBareiro <58647378+MBareiro@users.noreply.github.com>
2023-07-01 23:29:35 -0300

    Borrado de archivos innecesarios y carga del nuevo frontend

--- db/verificarUsuario.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    require('./conexionDb.php');
-    if(!isset($_SESSION['usuario']) && !isset($_SESSION['id_user'])){
-        header('location: ./logout.php');
-    }
-?>
--- email/php/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $conexion = new mysqli('localhost','root','','sistema');
-    if($conexion-> connect_error){
-        die('No se pudo conectar al servidor');
-    }
-
-?>
